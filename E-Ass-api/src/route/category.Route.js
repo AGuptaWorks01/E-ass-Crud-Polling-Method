b1d726ad70aed3557d4b61cba91542c5963c704a
Anurag <ganurag624outlook@gmail.com>
2025-04-16 16:07:24 +0530

    .

--- E-Ass-api/src/route/category.Route.js
+++ E-Ass-api/src/route/category.Route.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getcategories, InsertCategory, EditCategory, DeleteCategory, TruncateData } = require("../controller/category.Controller")
+const { getcategories, InsertCategory, EditCategory, DeleteCategory, TruncateData } = require("../controller/category.Controller");
+const authMiddleware = require("../middleware.js/auth.middleware");
 const router = express.Router();
 
-router.get("/categories", getcategories);
-router.post("/categories", InsertCategory);
-router.put("/categories/:id", EditCategory);
-router.delete("/categories/:id", DeleteCategory);
-router.delete("/categories", TruncateData);
+router.get("/categories", authMiddleware, getcategories);
+router.post("/categories", authMiddleware, InsertCategory);
+router.put("/categories/:id", authMiddleware, EditCategory);
+router.delete("/categories/:id", authMiddleware, DeleteCategory);
+router.delete("/categories", authMiddleware, TruncateData);
 
 module.exports = router
